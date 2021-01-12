090bf027535ed0a3548b6529b1d4250801eeb8f9
Anna Branco <annya.branco@gmail.com>
2021-01-12 13:33:48 +0100

    feat: [#124, #125] Time and round counting

---
+++ src/components/MainScreen/styles.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const MainArea = styled.main`
+  label: MainArea;
+  height: ${`${window.innerHeight}px`};
+  width: 100vw;
+  background: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
